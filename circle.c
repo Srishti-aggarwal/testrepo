1c373a75d9a26b1b9a390e1303d83324917dac63
Srishti-aggarwal <82658521+Srishti-aggarwal@users.noreply.github.com>
2021-06-10 23:03:05 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float radius;
+    printf("Enter the radius of circle: ");
+    scanf("%f",&radius);
+    float result1=3.14*radius*radius;
+    printf("Area of circle is %.2f\n",result1);
+    float result2=2*3.14*radius;
+    printf("Circumference of circle is %.2f",result2);
+    return 0;
+}
